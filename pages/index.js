c0bdcdc53f2c5cdcf49e91a56a95dbbe81788601
armandogutierrez4802 <armandogutierrez4802@gmail.com>
2023-09-11 15:31:25 -0700

    Initial commit

---
+++ pages/index.js
@@ -0,0 +1,17 @@
+import Head from 'next/head';
+import styles from '../styles/Home.module.css';
+
+export default function Home() {
+  return (
+    // <div className={styles.container}>
+    <div>
+      <Head>
+        <title>Home</title>
+      </Head>
+
+      <main>
+        <h1>Home Page</h1>
+      </main>
+    </div>
+  );
+}
