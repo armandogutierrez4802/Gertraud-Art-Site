274d67f3bd4a32d360d59cfbcb947f12e07b167b
armandogutierrez4802 <armandogutierrez4802@gmail.com>
2023-09-13 14:47:21 -0700

    Hero section done

--- pages/index.js
+++ pages/index.js
@@ -5,8 +5,7 @@ import styles from '../styles/Home.module.css';
 
 export default function Home() {
   return (
-    // <div className={styles.container}>
-    <div>
+    <>
       <Head>
         <title>Home</title>
       </Head>
@@ -15,16 +14,20 @@ export default function Home() {
       <Image
         src={HeroPhoto}
         alt="Hero photo"
-        // width={500} These 2 properties are not required since the images are statically imported
-        // height={500}
-        className='photo'
+        width={"auto"} //These 2 properties are not required since the images are statically imported
+        height={"auto"}
+        className={styles.hero}
       />
 
-      <main>
-        <h2>Hello</h2>
-        <h1><span className={styles.introSpan}>I am </span> <span className={styles.name}>Gertraud Schindelbacher</span></h1>
+      <main className={styles.main}>
+        <h6 className={styles.hello}>Hello</h6>
+        <h4 className={styles.h4}>I am <span className={styles.name}>Gertraud Schindelbacher</span></h4>
+        <p>Discover the glamour of my one-of-a-kind ceramics, meticulously crafted by skilled hands to ignite your imagination. Enter the world of Exclusive Ceramics, where imagination knows no limits. Explore my studio, admire the artistry, and stay updated on upcoming exhibitions you won't want to miss.</p>
       </main>
 
-    </div>
+      <div className='placeholder'>SCROLLING TEXT ANIMATION PLACEHOLDER</div>
+
+
+    </>
   );
 }
